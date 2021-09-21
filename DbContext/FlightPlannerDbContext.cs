a029771c880f3ee8ce467bcab73bdf5caec167ba
norisimons <42768523+norisimons@users.noreply.github.com>
2021-09-21 17:56:04 +0300

    .

---
+++ DbContext/FlightPlannerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FlightPlanner.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightPlanner.Web.DbContext
+{
+    public class FlightPlannerDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public FlightPlannerDbContext(DbContextOptions<FlightPlannerDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Airport> Airport { get; set; }
+
+    }
+}
