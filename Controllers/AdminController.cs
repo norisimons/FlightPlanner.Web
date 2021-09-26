9b281e3bc5c6aaf80264f3ef68a5c019fbc16644
norisimons <42768523+norisimons@users.noreply.github.com>
2021-09-27 00:44:42 +0300

    .

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using FlightPlanner.Web.Storage;
 using FlightPlanner.Web.Models;
